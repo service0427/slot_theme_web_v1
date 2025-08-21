fd35634488747b6eb06e7c057c7b2c4426b43545
jicho <milinda@naver.com>
2025-08-21 22:50:01 +0900

    [jicho] 폰트 변경 4차

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'system-ui', 'sans-serif'],
+        'inter': ['Inter', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'system-ui', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
